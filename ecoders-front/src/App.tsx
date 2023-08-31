0b2b1d6c85970b1b73368680e4e2f11ec4bd98d1
strong-minsu <dlalstn5789@gmail.com>
2023-08-31 14:08:43 +0900

    style : 글 작성 페이지 생성

--- ecoders-front/src/App.tsx
+++ ecoders-front/src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import CommunityPage from './pages/CommunityPage';
+import CommunityWritePage from './pages/CommunityWritePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={Home} />
-        <Route element={NotFound} /> */}
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/community/write" element={<CommunityWritePage />} />
+        {/* <Route element={NotFound} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ ecoders-front/src/pages/CommunityWritePage.tsx
@@ -0,0 +1,5 @@
+function CommunityWritePage() {
+  return <div>글 작성 페이지 입니다.</div>;
+}
+
+export default CommunityWritePage;

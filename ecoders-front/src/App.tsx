b37308a6ed7111185ffad6c0a556f52938070fcf
strong-minsu <dlalstn5789@gmail.com>
2023-08-31 16:48:52 +0900

    app.tsx 수정

--- ecoders-front/src/App.tsx
+++ ecoders-front/src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 import './App.css';
 

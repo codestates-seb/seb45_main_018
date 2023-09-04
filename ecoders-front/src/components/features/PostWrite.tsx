ce628b72ab41aec7190d85ad783a863733f7e64e
strong-minsu <dlalstn5789@gmail.com>
2023-09-04 16:41:05 +0900

    quill 코드 삭제

--- ecoders-front/src/components/features/PostWrite.tsx
+++ ecoders-front/src/components/features/PostWrite.tsx
@@ -7,49 +7,6 @@ import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import CommonButton from '../atoms/CommonButton';
-// const EditorComponent = () => {
-//   const QuillRef = useRef<ReactQuill>();
-//   const [contents, setContents] = useState('');
-
-//   // quill에서 사용할 모듈을 설정하는 코드 입니다.
-//   // useMemo를 사용하지 않으면, 키를 입력할 때마다, imageHandler 때문에 focus가 계속 풀리게 됩니다.
-//   const modules = useMemo(
-//     () => ({
-//       toolbar: {
-//         container: [
-//           ['link', 'image', 'video'],
-//           [{ header: [1, 2, 3, false] }],
-//           ['bold', 'italic', 'underline', 'strike'],
-//           ['blockquote'],
-//           [{ list: 'ordered' }, { list: 'bullet' }],
-//           [{ color: [] }, { background: [] }],
-//           [{ align: [] }],
-//         ],
-//         // handlers: {
-//         //   image: imageHandler,
-//         // },
-//       },
-//     }),
-//     [],
-//   );
-
-//   return (
-//     <>
-//       <ReactQuill
-//         ref={element => {
-//           if (element !== null) {
-//             QuillRef.current = element;
-//           }
-//         }}
-//         value={contents}
-//         onChange={setContents}
-//         modules={modules}
-//         theme="snow"
-//         placeholder="내용을 입력해주세요."
-//       />
-//     </>
-//   );
-// };
 
 const PostWriteHeader = () => {
   return (

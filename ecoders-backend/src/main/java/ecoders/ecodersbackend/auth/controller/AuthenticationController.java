e631f5881e63a4a3e0efe531f3435c989ba2a4cb
blacksg <sagwa.nth@gmail.com>
2023-09-19 01:29:00 +0900

    [Refactor] JWT 토큰 발급 로직 중복 제거

--- ecoders-backend/src/main/java/ecoders/ecodersbackend/auth/controller/AuthenticationController.java
+++ ecoders-backend/src/main/java/ecoders/ecodersbackend/auth/controller/AuthenticationController.java
@@ -5,8 +5,6 @@ import ecoders.ecodersbackend.auth.mapper.AuthMapper;
 import ecoders.ecodersbackend.domain.member.entity.Member;
 import ecoders.ecodersbackend.domain.member.service.MemberService;
 import ecoders.ecodersbackend.util.UriCreator;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +19,6 @@ import java.util.UUID;
 
 import static ecoders.ecodersbackend.auth.dto.AuthDto.GoogleLoginDto;
 import static ecoders.ecodersbackend.auth.dto.AuthDto.SignupDto;
-import static ecoders.ecodersbackend.auth.jwt.JwtProvider.HEADER_AUTHORIZATION;
-import static ecoders.ecodersbackend.auth.jwt.JwtProvider.HEADER_REFRESH_TOKEN;
 
 @AllArgsConstructor
 @RequestMapping("/auth")
@@ -47,16 +43,6 @@ public class AuthenticationController {
     public void googleLogin(@RequestBody GoogleLoginDto googleLoginDto, HttpServletResponse response) {
         Member member = authMapper.googleLoginDtoToMember(googleLoginDto);
         Member signedInMember = memberService.googleLogin(member);
-
-        String memberId = signedInMember.getId().toString();
-        String email = signedInMember.getEmail();
-        boolean isVerified = signedInMember.isVerified();
-        Claims claims = Jwts.claims().setSubject(memberId);
-        claims.put("email", email);
-        claims.put("isVerified", isVerified);
-
-        response.setHeader(HEADER_AUTHORIZATION, jwtProvider.generateAccessToken(claims));
-        response.setHeader(HEADER_REFRESH_TOKEN, jwtProvider.generateRefreshToken(claims));
-        response.setHeader("Member-ID", memberId);
+        jwtProvider.issueTokens(signedInMember, response);
     }
 }
